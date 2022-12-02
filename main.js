d32a2dc0be53dbf62e6e933c8a24e6194801605c
Kindziolek <kingapietron@gmail.com>
2022-12-02 15:35:16 +0100

    table

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 const button = document.querySelector("button");
 
-button.addEventListener("cilsk", function () {
+function renderTable(users) {
+    const table = `
+    <table class="table is-fullwidth is-bordered is-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Imię i nazwisko</th>
+                <th>Nazwa użytkownika</th>
+                <th>Adres e-mail</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+     </table>
+     `;
 
+     const div = document.createElement("div");
+     
+     div.innerHTML = table;
+}
+
+button.addEventListener("cilck", function () {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => renderTable(data));
 });
